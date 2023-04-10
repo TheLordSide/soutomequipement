5623b731aca7bfbdeec93c748092785041b96861
TheLordSide <cedricaguigah@gmail.com>
2023-04-10 13:21:32 +0000

    ajout db,datamodule

--- Login.pas
+++ Login.pas
@@ -4,14 +4,16 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Vcl.Imaging.pngimage;
 
 type
   TF_login = class(TForm)
     username: TEdit;
     password: TEdit;
-    Panel1: TPanel;
+    Panel_login: TPanel;
     Button1: TButton;
+    Image1: TImage;
   private
     { Déclarations privées }
   public
